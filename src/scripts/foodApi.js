edb67c0961aa60f04072ecbc4cf041507b481f63
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-04-18 09:51:39 -0500

    fixing last of undefines on DOM

---
+++ src/scripts/foodApi.js
@@ -0,0 +1,50 @@
+fetch("http://localhost:8088/food")
+    .then(foods => foods.json())
+    .then(parsedFoods => {
+        console.table(parsedFoods)
+        parsedFoods.forEach(food => {
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+            .then(response => response.json())
+                .then(productInfo => {
+                    console.log(productInfo)
+                    let productDetails = grabInfo(productInfo.product.ingredients_text, productInfo.product.countries, productInfo.product.nutriments.energy, productInfo.product.nutriments.fat, productInfo.product.nutriments.sugars)
+                    const foodAsHtml = foodFactory(food.name, food.type, food.ethnicity, productDetails);
+                    addFoodToContainer(foodAsHtml)
+                });
+        });
+    });
+
+const h1 = (title, style) => {
+    return `<h1 class="${style}">${title}</h1>`;
+};
+
+const h3 = (title, style) => {
+    return `<h3 class="${style}">${title}</h3>`;
+};
+
+const p = (ingredientsList, ethnicity, productDetails) => {
+    return `<p>${ingredientsList}, ${ethnicity}, ${productDetails}.</p>`;
+}
+
+const div = (name, type, ethnicity, productDetails, style) => {
+    return `<div class="${style}">
+            ${h1(name, "h1-style")}
+            ${h3(type, "h3-style")}
+            ${h3(ethnicity, "h3-style")}
+            ${p(productDetails)}
+            </div>`;
+};
+
+let foodFactory = (name, type, ethnicity, productDetails) => {
+    let factoryOutput = div(name, type, ethnicity, productDetails, "div-style");
+    return `${factoryOutput}`;
+};
+
+let addFoodToContainer = (item) => {
+    document.querySelector("#container").innerHTML += item;
+}
+
+let grabInfo = (...items) => {
+    console.log(items)
+    return items;
+};
